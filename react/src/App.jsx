ace94bcda0eeb62ac7e20b1a3f373d7216f3129c
Aadil Sheriff <aadilsheriff@Aadils-MacBook-Air.local>
2023-12-31 18:29:39 +0530

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navigation from "./components/navigation";
+import Hero from "./components/Hero";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <Hero/>
+    </div>
+  );
+};
+
+export default App;
